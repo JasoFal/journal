774c442a118c29fa8cd73566f8b492a4be6b08dd
Jason Falk <jasonfalk9@gmail.com>
2023-06-30 16:09:31 -0700

    Change template name to journal.

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "template-repo",
+      title: "journal",
       template: "./src/index.html",
       inject: "body"
     })
